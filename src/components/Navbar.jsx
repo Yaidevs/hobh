860450a0cb3b6305c393b92fa2862395baebc8ba
abel <abelkinfu678@gmail.com>
2024-12-23 16:13:05 +0300

    logo is inserted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
-import logo from '../assets/logo.png';
+import logo from '../assets/logo2.jpg';
 
 export function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white py-1 px-6 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-white px-6 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <a href="/" className="font-bold text-3xl">
-          <img src={logo} className="w-20 h-20" alt="Logo" />
+          <img src={logo} className="w-24 h-16" alt="Logo" />
         </a>
         <div className="space-x-2">
           <a
             href="/login"
-            className="px-4 py-2 font-semibold rounded-md hover:bg-gray-100 transition-colors"
+            className="px-4 py-2 font-medium rounded-md hover:bg-gray-100 transition-colors"
           >
             Log in
           </a>
           <a
             href="/signup"
-            className="px-4 py-2 font-semibold rounded-md hover:bg-gray-100 transition-colors"
+            className="px-4 py-2 font-medium rounded-md hover:bg-gray-100 transition-colors"
           >
             Sign up
           </a>
